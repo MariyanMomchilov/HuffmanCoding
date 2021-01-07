0728f1445c55391b41630675223e910af0ff348f
Mariyan Momchilov <mariyandge@gmail.com>
2021-01-07 20:04:20 +0200

    cleaned up main

--- HuffmanCodingProject/src/main.cpp
+++ HuffmanCodingProject/src/main.cpp
@@ -1,30 +1,11 @@
-#define TEST_MODE 0
-#define DEBUG_MODE 0
-
-#if DEBUG_MODE
-#include "PriorityQueue.cpp"
-#include "HuffmanTree.cpp"
-#include "HuffmanCode.cpp"
-#include "HuffmanCallable.cpp"
-#include "Builder.cpp"
-#include "CLI.cpp"
-#else
 #include "../include/Node.h"
 #include "../include/PriorityQueue.h"
 #include "../include/HuffmanTree.h"
 #include "../include/HuffmanCode.h"
 #include "../include/CLI.h"
-#endif
-
-#if TEST_MODE
-#include "../include/tests.cpp"
-#endif
 
 int main(int argc, char *argv[])
 {
-#if TEST_MODE
-    doctest::Context().run();
-#else
     CLI cmd(argc, argv);
     try
     {
@@ -34,7 +15,6 @@ int main(int argc, char *argv[])
     {
         std::cerr << e.what() << '\n';
     }
-#endif
 
     return 0;
 }
